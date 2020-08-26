0a80925718f7ecb44fd96a57a2d7e05485d86eec
Rizwan Ahmad <rizwan-ak>
2020-08-26 15:05:10 +0500

    Updates

---
+++ precache-manifest.7072ad1b2d866f0f9e57fa18f4db8d00.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf6e7b4961feafdf12018738c3cc118",
+    "url": "/quickwashAdmin/index.html"
+  },
+  {
+    "revision": "8f0c9376eafedf7e21a3",
+    "url": "/quickwashAdmin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d1a01a62002b70f32ecb",
+    "url": "/quickwashAdmin/static/css/main.4ac486a4.chunk.css"
+  },
+  {
+    "revision": "8f0c9376eafedf7e21a3",
+    "url": "/quickwashAdmin/static/js/2.5bf9d19e.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/quickwashAdmin/static/js/2.5bf9d19e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a01a62002b70f32ecb",
+    "url": "/quickwashAdmin/static/js/main.955eb909.chunk.js"
+  },
+  {
+    "revision": "e6553d95c6ffaa34794b",
+    "url": "/quickwashAdmin/static/js/runtime-main.37e1f211.js"
+  },
+  {
+    "revision": "1ee966d656f02e2e41912d7622009854",
+    "url": "/quickwashAdmin/static/media/dp.1ee966d6.png"
+  },
+  {
+    "revision": "9f6e26d5b81fff1847dc55e1a4207e43",
+    "url": "/quickwashAdmin/static/media/greenLogo.9f6e26d5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quickwashAdmin/precache-manifest.8d8c5b8a49ee52969ad871a8afbc34f0.js"
+  "/quickwashAdmin/precache-manifest.7072ad1b2d866f0f9e57fa18f4db8d00.js"
 );
 
 self.addEventListener('message', (event) => {
